5019864d4ae50f21e059b6bbc8acec7dfc96ac59
Karthikeya G <karthikeya.vamsi123@gmail.com>
2018-02-05 20:37:09 -0500

    added production changes

--- index.js
+++ index.js
@@ -22,5 +22,16 @@ app.use(passport.session());
 authRoutes(app);
 mongoose.connect(keys.mongoURI);
 
+if (process.env.NODE_ENV=='production'){
+	//express will serve up production assets like main.js or main.css
+	app.use(express.static('client/build'));
+
+	//express will serve up the index.html, if it doesn't recognise the route
+	const path= require('path');
+	app.get('*', (req, res)=>{
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT= process.env.PORT || 5000;
 app.listen(PORT);
